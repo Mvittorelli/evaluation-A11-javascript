61d6a0c5276b8959ce02a0f6f7cefe9fff26e22c
Mvittorelli <mvittorelli@edenschool.fr>
2024-11-15 14:50:03 +0100

    i <3 cat

--- ex07.js
+++ ex07.js
@@ -3,7 +3,7 @@ function isNationalIDValid(name, age, zip, password) {
 	if (name === "") {
 		return false;
 	} else if (age >= 18 && age <= 65) {
-		if (zip <= 99999 && zip >= 0) {
+		if (zip <= 99999 && zip > 0) {
 			if (password != "qwerty" || password != "azerty" || password != 1234) {
 				return true;
 			} else {
