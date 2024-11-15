39dbcdf3c48af05ca7a2609c45f30ca9f6eb9e73
Mvittorelli <mvittorelli@edenschool.fr>
2024-11-15 14:00:37 +0100

    feat(app):first commit

---
+++ ex06.js
@@ -0,0 +1,14 @@
+function getReduction(price, age) {
+	if (age <= 5){
+      return (price/100)*0;
+    } else if(age <= 18){
+      return (price/100)*50;
+    } else if (age <= 25){
+      return (price/100)*30;
+    } else {
+      return price;
+    }
+}
+
+console.log(getReduction(10, 5)); // Résultat attendu: 0
+console.log(getReduction(150, 25)); // Résultat attendu: 45
